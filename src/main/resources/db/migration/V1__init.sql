fd3c5e6438f27eb22b5b3099f7ab6dfafc263286
Wilson Castillo <dc105677543@gmail.com>
2022-12-07 10:03:35 -0500

    Piensa

--- src/main/resources/db/migration/V1__init.sql
+++ src/main/resources/db/migration/V1__init.sql
@@ -18,7 +18,25 @@ CREATE TABLE IF NOT EXISTS preguntas{
 id SERIAL,
 pregunta VARCHAR (100) NOT NULL,
 respuesta VARCHAR (100) NOT NULL,
-PRIMARY KEY (id)
+cuestionario_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY cuestionario_id REFERENCES cuestionario (id)
+};
+
+CREATE TABLE IF NOT EXISTS clases{
+id SERIAL,
+descripcion VARCHAR (100) NOT NULL,
+link VARCHAR (100) NOT NULL,
+duracion DECIMAL (10,12)
+};
+
+CREATE TABLE IF NOT EXISTS estudiante_cuestionario{
+id SERIAL,
+estudiante_id INT NOT NULL,
+cuestionario_id INT NOT NULL,
+PRIMARY KEY (id),
+FOREIGN KEY estudiante_id REFERENCES estudiante (id),
+FOREIGN KEY cuestionario_id REFERENCES cuestionario (id)
 };
 
 
